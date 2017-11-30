87e5d7900c9d789f45c64776abf4f1dc22b2f25e
Hyftar <simon_landry@ymail.com>
2017-11-30 02:32:52 -0500

    Move and standardize the title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def application_title
+    Rails.application.class.parent_name.titleize.split.join('-')
+  end
+
+  def full_title(title)
+    title.empty? ? application_title : "#{title} | #{application_title}"
+  end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,2 @@
 module StaticPagesHelper
-
-  def full_title(title)
-    @title = 'Bolty-box'
-    if title == ""
-      title
-    else
-      "#{title}-#{@title}"
-    end
-  end
 end
