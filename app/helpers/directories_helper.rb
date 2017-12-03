b903bc1eb82ee516d913840956d57cf6fc2d0e8a
Billy <billy.bouchard03@gmail.com>
2017-12-03 01:18:33 -0500

    Started assets partial viex for index

--- app/helpers/directories_helper.rb
+++ app/helpers/directories_helper.rb
@@ -3,7 +3,15 @@ module DirectoriesHelper
     directory.public? ? 'public' : 'private'
   end
 
+  def asset_public_text(asset)
+    asset.directory.public? ? 'public' : 'private'
+  end
+
   def owner_alias(directory)
     directory.owner == current_user ? 'me' : directory.owner.full_name
+  end
+
+  def asset_owner_alias(asset)
+    asset.directory.owner == current_user ? 'me' : asset.directory.owner.full_name
   end
 end
