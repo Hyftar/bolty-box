82a7ebad2872469d71620dc75f9388590fd2dcd4
Hyftar <simon_landry@ymail.com>
2017-12-07 22:13:39 -0500

    Remove broken helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   validates :last_name, presence: true, format: { with: /\A[[:alpha:]\- ]{2,}\z/ }
 
   has_and_belongs_to_many :directories
-  has_many :assets, through: :directories
 
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
